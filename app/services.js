fc2f079fe65eb73f272a92db7efadd851b8debff
Arun Gupta <guptaarun@hotmail.com>
2013-09-04 16:00:00 +0530

    First commit - v2 - level 2 - Drag drop

--- app/services.js
+++ app/services.js
@@ -12,7 +12,10 @@ demoApp.service('demoService', function () {
 			{name:'Sample 2', id:'2', url:'page2'}, 
 			{name:'Sample 3', id:'3', url:'page3'}, 
 		]},
-		{name:'Level 2', id:'2', pages:[]},
+		{name:'Level 2', id:'2', pages:[
+			{name:'Sample 1', id:'1', url:'page1'}, 
+
+		]},
 		{name:'Level 3', id:'3', pages:[]}
 	];
 
