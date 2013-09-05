f7e2c39eb9a8c86e176b7400ef731be5eb0f4941
Arun Gupta <guptaarun@hotmail.com>
2013-09-05 11:53:46 +0530

    Added pages for level 2

--- app/services.js
+++ app/services.js
@@ -14,6 +14,8 @@ demoApp.service('demoService', function () {
 		]},
 		{name:'Level 2', id:'2', pages:[
 			{name:'Sample 1', id:'1', url:'page1'}, 
+			{name:'Sample 2', id:'2', url:'page2'}, 
+			{name:'Sample 3', id:'3', url:'page3'}
 
 		]},
 		{name:'Level 3', id:'3', pages:[]}
